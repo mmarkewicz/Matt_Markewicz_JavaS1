c63beed4bcd3734074b0f44b14d1a33d56ab9343
Matt M <42874279+mmarkewicz@users.noreply.github.com>
2019-09-23 19:19:43 -0400

    hw1

---
+++ 20190923/katas/starter-code/U1M1L1Katas/src/main/java/com/company/MarvelTable.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class MarvelTable {
+
+    public static void main(String[] args) {
+        System.out.print("-------------------------------------------------------------\n" +
+                "|   Hero              |   Affiliation     |   Power         |\n" +
+                "-------------------------------------------------------------\n" +
+                "|   Captain America   |   The Avengers    |   Peak Human    |\n" +
+                "-------------------------------------------------------------\n" +
+                "|   Wolverine         |   The X-Men       |   Healing/Claws |\n" +
+                "-------------------------------------------------------------\n" +
+                "|   Cyclops           |   The X-Men       |   Eye Beam      |\n" +
+                "-------------------------------------------------------------\n" +
+                "|   Domino            |   X-Force         |   Luck          |\n" +
+                "-------------------------------------------------------------\n");
+    }
+}
