7991cdb5009033fba5cad5cca9dcfb6566859435
Matt M <42874279+mmarkewicz@users.noreply.github.com>
2019-09-24 18:23:28 -0400

    activities pushed

--- 20190925/02-registration-form/starter-code/CommandLineGram/src/main/java/com/company/CommandLineGram.java
+++ 20190925/02-registration-form/starter-code/CommandLineGram/src/main/java/com/company/CommandLineGram.java
@@ -1,8 +1,66 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class CommandLineGram {
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.println("First Name: ");
+        String firstName = scanner.next();
+
+        System.out.println("Last Name: ");
+        String lastName = scanner.next();
+
+        System.out.println("Email: ");
+        String email = scanner.next();
+
+        System.out.println("Twitter Handle: ");
+        String handle = scanner.next();
+
+        System.out.println("Age: ");
+        int age = scanner.nextInt();
+
+        System.out.println("Country: ");
+        String country = scanner.next();
+
+        System.out.println("Profession: ");
+        scanner.nextLine();
+        String profession = scanner.nextLine();
+
+        System.out.println("Favorite OS: ");
+        String favoriteOS = scanner.next();
+
+        System.out.println("Favorite Language: ");
+        String favoriteLanguage = scanner.next();
+
+        System.out.println("Favorite Comp Scientist: ");
+        scanner.nextLine();
+        String favoriteScientist = scanner.nextLine();
+
+        System.out.println("Favorite Keyboard Shortcut: ");
+        String favoriteShortcut = scanner.next();
+
+        System.out.println("Have you ever built your own computer?");
+        String everBuilt = scanner.next();
+
+        System.out.println("If you could be any superhero, who would it be? ");
+        scanner.nextLine();
+        String superhero = (scanner.nextLine());
+
+        System.out.println("1. " + firstName);
+        System.out.println("2. " + lastName);
+        System.out.println("3. " + email);
+        System.out.println("4. " + handle);
+        System.out.println("5. " + age);
+        System.out.println("6. " + country);
+        System.out.println("7. " + profession);
+        System.out.println("8. " + favoriteOS);
+        System.out.println("9. " + favoriteLanguage);
+        System.out.println("10. " + favoriteScientist);
+        System.out.println("11. " + favoriteShortcut);
+        System.out.println("12. " + everBuilt);
+        System.out.println("13. " + superhero);
     }
 }
--- 20190925/03-luxury-tax-calculator/starter-code/LuxuryTaxCalculator/src/main/java/com/company/LuxuryTaxCalculator.java
+++ 20190925/03-luxury-tax-calculator/starter-code/LuxuryTaxCalculator/src/main/java/com/company/LuxuryTaxCalculator.java
@@ -1,8 +1,39 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class LuxuryTaxCalculator {
 
     public static void main(String[] args) {
+        // prompt user to enter the salary for player 1, player 2, and player 3 (without $ or commas)
+        // calculate and display total salary for all 3 people
+        // if team went over spending limit, calculate and display cost of luxury tax
+        // spending limit is $40million
+        // lux tax rate is 18%, and is only paid on the amount of salary over the team's spending limit
+        // ex: if team payroll is $55million, they only pay 18% of the $15million they are over the cap, not the entire salary
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter Player 1's Salary: ");
+        int playerOneSal = scanner.nextInt();
+
+        System.out.println("Enter Player 2's Salary: ");
+        int playerTwoSal = scanner.nextInt();
+
+        System.out.println("Enter Player 3's Salary: ");
+        int playerThreeSal = scanner.nextInt();
+
+        int totalSalary = playerOneSal + playerTwoSal + playerThreeSal;
+        System.out.println("Total Salary: " + totalSalary);
+
+        if (totalSalary > 45000000) {
+            int taxableTotal = totalSalary - 45000000;
+            System.out.println("Taxable Total: " + taxableTotal);
+            float totalTax = 0.24f * taxableTotal;
+            System.out.println("Total Tax Owed: " + totalTax);
+        } else {
+            System.out.println("There is no luxury tax");
+        }
 
     }
 }
