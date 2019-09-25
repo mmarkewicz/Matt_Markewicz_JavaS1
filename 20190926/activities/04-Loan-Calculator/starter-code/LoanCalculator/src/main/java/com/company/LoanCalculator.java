0a40161c63487291e57c2cf04ab2c115c9cba5cc
Matt M <42874279+mmarkewicz@users.noreply.github.com>
2019-09-25 15:22:33 -0400

    prime checker

--- 20190926/activities/04-Loan-Calculator/starter-code/LoanCalculator/src/main/java/com/company/LoanCalculator.java
+++ 20190926/activities/04-Loan-Calculator/starter-code/LoanCalculator/src/main/java/com/company/LoanCalculator.java
@@ -7,17 +7,26 @@ public class LoanCalculator {
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Enter Amount of Mortgage: ");
-        float total = scanner.nextFloat();
+        float L = scanner.nextFloat();
 
         System.out.println("Enter Term of Mortgage: ");
-        int term = scanner.nextInt();
+        int n = scanner.nextInt();
 
         System.out.println("Enter Annual Interest Rate of Mortgage");
-        int annualInterestRate = scanner.nextInt();
+        float interest = scanner.nextFloat();
 
-        int monthlyInterestRate = (annualInterestRate / 100) / 12;
-        float monthlyPayment = monthlyInterestRate * total;
-        System.out.println("Monthly Payment: " + monthlyPayment);
+        float c =  interest / 12;
+
+        // P = L[c(1 + c)n]/[(1 + c)n - 1]
+        // P = Fixed Monthly Payment, L = Total Dollars, n = number of months, c = monthly interest rate
+        float onePlusC = 1.00f + c;
+
+        float numerator = c * onePlusC * n;
+        float denominator = onePlusC * n;
+        float dividedMinusOne = (numerator / denominator) - 1;
+        float calculated = L * dividedMinusOne;
+
+        System.out.println("Calculated Monthly Payment: " + calculated);
 
     }
 }
--- 20190926/activities/05-Prime-Finder/starter-code/PrimeFinder/src/main/java/com/company/PrimeFinder.java
+++ 20190926/activities/05-Prime-Finder/starter-code/PrimeFinder/src/main/java/com/company/PrimeFinder.java
@@ -1,7 +1,30 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class PrimeFinder {
     public static void main(String[] args) {
+        // take in user input as ceiling (inclusive) and print all prime numbers from 1 to the input
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.println("Enter a number: ");
+        // ex: 10
+        int userInput = scanner.nextInt();
+
+        // iterate through each number before userInput
+        for (int i = 2; i <= userInput; i++) {
+            boolean isPrime = true;
+            // test current number for primeness
+            for (int j = 2; j < i; j++) {
+                // if number divided by iterator leaves remainder of 0, number is not prime
+                if (i % j == 0) {
+                    isPrime = false;
+                }
+            }
+            if (isPrime) {
+                System.out.println(i);
+            }
+
+        }
     }
 }
