e51084eba9445e92f320f5cc4ef39acb85204304
Matt M <42874279+mmarkewicz@users.noreply.github.com>
2019-09-25 10:26:15 -0400

    9.26 push

---
+++ 20190926/activities/01-Hi-Lo/starter-code/HiLo/src/main/java/com/company/HiLo.java
@@ -0,0 +1,43 @@
+package com.company;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class HiLo {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // random number generator
+        Random rand = new Random();
+        int low = 1;
+        int high = 101;
+        int result = rand.nextInt(high-low) + low;
+
+        // console instructions
+        System.out.println("Welcome to Hi-Low!");
+        System.out.println("Enter Username: ");
+        String username = scanner.next();
+
+        System.out.println("OK, " + username + ", here are the rules:");
+
+        boolean isUserCorrect = false;
+        int numGuesses = 1;
+
+        while(isUserCorrect == false) {
+            System.out.println("Pick a number between 1 and 100");
+            int userSelection = scanner.nextInt();
+
+            if (userSelection == result) {
+                System.out.println("Congratulations, " + username + "! You win!");
+                System.out.println("It took you " + numGuesses + " guesses to find my number!");
+                isUserCorrect = true;
+                break;
+            } else if (userSelection > result) {
+                System.out.println("Too high");
+            } else if (userSelection < result) {
+                System.out.println("Too low");
+            }
+        }
+
+    }
+}
