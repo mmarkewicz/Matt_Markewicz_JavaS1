8ca4ce34198869567f2c852d5a5ba5142454b248
Matt M <42874279+mmarkewicz@users.noreply.github.com>
2019-09-26 08:35:44 -0400

    push changes

--- 20190926/activities/05-Prime-Finder/starter-code/PrimeFinder/src/main/java/com/company/PrimeFinder.java
+++ 20190926/activities/05-Prime-Finder/starter-code/PrimeFinder/src/main/java/com/company/PrimeFinder.java
@@ -4,16 +4,16 @@ import java.util.Scanner;
 
 public class PrimeFinder {
     public static void main(String[] args) {
-        // take in user input as ceiling (inclusive) and print all prime numbers from 1 to the input
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Enter a number: ");
-        // ex: 10
-        int userInput = scanner.nextInt();
+        System.out.println("Enter a number: "); // terminal prompt
+        int userInput = scanner.nextInt(); // save user input
 
         // iterate through each number before userInput
         for (int i = 2; i <= userInput; i++) {
-            boolean isPrime = true;
+
+            boolean isPrime = true; // set each integer to prime initially
+
             // test current number for primeness
             for (int j = 2; j < i; j++) {
                 // if number divided by iterator leaves remainder of 0, number is not prime
@@ -21,6 +21,8 @@ public class PrimeFinder {
                     isPrime = false;
                 }
             }
+
+            // if the isPrime bool is true, print the current value of i
             if (isPrime) {
                 System.out.println(i);
             }
