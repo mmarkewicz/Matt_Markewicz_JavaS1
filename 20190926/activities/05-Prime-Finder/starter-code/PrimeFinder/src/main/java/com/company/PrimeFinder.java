ac326ff2b9741b21fdd320ce487000ef60988ca2
Matt M <42874279+mmarkewicz@users.noreply.github.com>
2019-09-27 00:17:35 -0400

    prime check method

--- 20190926/activities/05-Prime-Finder/starter-code/PrimeFinder/src/main/java/com/company/PrimeFinder.java
+++ 20190926/activities/05-Prime-Finder/starter-code/PrimeFinder/src/main/java/com/company/PrimeFinder.java
@@ -3,6 +3,19 @@ package com.company;
 import java.util.Scanner;
 
 public class PrimeFinder {
+
+    public static void checkIfPrimeAndPrint(int num) {
+        boolean isPrime = true;
+        for (int i = 2; i < num; i++) {
+            if (num % i == 0) {
+                isPrime = false;
+            }
+        }
+        if (isPrime) {
+            System.out.println(num);
+        }
+    }
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
@@ -11,22 +24,7 @@ public class PrimeFinder {
 
         // iterate through each number before userInput
         for (int i = 2; i <= userInput; i++) {
-
-            boolean isPrime = true; // set each integer to prime initially
-
-            // test current number for primeness
-            for (int j = 2; j < i; j++) {
-                // if number divided by iterator leaves remainder of 0, number is not prime
-                if (i % j == 0) {
-                    isPrime = false;
-                }
-            }
-
-            // if the isPrime bool is true, print the current value of i
-            if (isPrime) {
-                System.out.println(i);
-            }
-
+            PrimeFinder.checkIfPrimeAndPrint(i);
         }
     }
 }
