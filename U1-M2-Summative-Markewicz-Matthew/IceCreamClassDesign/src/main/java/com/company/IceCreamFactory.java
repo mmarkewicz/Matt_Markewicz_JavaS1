a2374aa06e85449fad3a02cff2119e1cb9870e29
Matt M <42874279+mmarkewicz@users.noreply.github.com>
2019-10-04 14:52:40 -0400

    ice cream

---
+++ U1-M2-Summative-Markewicz-Matthew/IceCreamClassDesign/src/main/java/com/company/IceCreamFactory.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class IceCreamFactory {
+    // attributes of all ice cream
+    private String flavor;
+    private double quantity;
+    private String expirationDate;
+
+    // factory specific attributes
+    private double costOfProduction;
+    private boolean isShipped;
+    private String shippingDestination;
+    private String originFactory;
+}
---
+++ U1-M2-Summative-Markewicz-Matthew/IceCreamClassDesign/src/main/java/com/company/IceCreamPoS.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class IceCreamPoS {
+    // attributes of all ice cream
+    private String flavor;
+    private double quantity;
+    private String expirationDate;
+
+    // PoS specific attributes
+    private double costFromWholesaler;
+    private double priceReceived;
+    private boolean isSold;
+}
