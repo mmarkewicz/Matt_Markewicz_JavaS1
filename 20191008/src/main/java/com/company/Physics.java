4848457703cf07601fcab307614b1071380f62c5
Matt M <42874279+mmarkewicz@users.noreply.github.com>
2019-10-08 08:38:50 -0400

    update

--- 20191008/src/main/java/com/company/Physics.java
+++ 20191008/src/main/java/com/company/Physics.java
@@ -3,19 +3,35 @@ package com.company;
 public class Physics {
 
     public double calculateForce(double mass, double acceleration) {
-        return mass * acceleration;
+        if (acceleration == 0) {
+            return 0;
+        } else {
+            return mass * acceleration;
+        }
     }
 
     public double calculateVelocity(double distance1, double distance2, double time1, double time2) {
-        return (distance2 - distance1) / (time2 - time1);
+        if (time2 - time1 == 0) {
+            return 0;
+        } else {
+            return (distance2 - distance1) / (time2 - time1);
+        }
     }
 
     public double calculateMass(double weight, double gravity) {
-        return weight / gravity;
+        if (gravity == 0) {
+            return 0;
+        } else {
+            return weight / gravity;
+        }
     }
 
     public double calculateAcceleration(double velocity1, double velocity2, double time1, double time2) {
-        return (velocity2 - velocity1) / (time2 - time1);
+        if (time2 - time1 == 0) {
+            return 0;
+        } else {
+            return (velocity2 - velocity1) / (time2 - time1);
+        }
     }
 
     public double calculateDistance(double velocity, double time) {
--- 20191008/src/test/java/com/company/PhysicsTest.java
+++ 20191008/src/test/java/com/company/PhysicsTest.java
@@ -17,11 +17,36 @@ public class PhysicsTest {
     public void shouldCalculateForceReturnProduct() {
         assertEquals(100, physics.calculateForce(10, 10), 0.1);
         assertEquals(500.00d, physics.calculateForce(50.0d, 10.0d), 0.1);
+        assertEquals(0, physics.calculateForce(50.00d, 0), 0.0); // if denominator = 0, return 0
     }
 
     @Test
     public void shouldCalculateVelocityReturnDivision() {
         assertEquals(100.00d, physics.calculateVelocity(500.00d, 600.00d, 5.00d, 6.00d), 0.1);
+        assertEquals(0d, physics.calculateVelocity(500.00d, 600.00d, 5.00d, 5.00d), 0.1); // if dividing by 0, return 0
+        assertEquals(-1100.00d, physics.calculateVelocity(500.00d, -600.00d, 5.00d, 6.00d), 0.1);
     }
+
+    @Test
+    public void shouldCalculateMassReturnWeightDividedByGravity() {
+        assertEquals(100, physics.calculateMass(100, 1), 0.1);
+        assertEquals(0, physics.calculateMass(100, 0), 0.1); // if denominator = 0; return 0
+        assertEquals(0, physics.calculateMass(0, 100), 0.1);
+    }
+
+    @Test
+    public void shouldCalculateAccelerationReturnVelocityDeltaDividedByTimeDelta() {
+        assertEquals(100, physics.calculateAcceleration(300, 400, 9, 10), 0.1);
+        assertEquals(0, physics.calculateAcceleration(500, 600, 10, 10), 0.1);
+        assertEquals(0, physics.calculateAcceleration(600, 600, 10, 9), 0.1);
+    }
+
+    @Test
+    public void shouldCalculateDistanceReturnVelocityTimesTime() {
+        assertEquals(100, physics.calculateDistance(10, 10), 0.0001);
+        assertEquals(0, physics.calculateDistance(0, 100), 0.0001);
+        assertEquals(0, physics.calculateDistance(100, 0), 0.0001);
+    }
+
 
 }
