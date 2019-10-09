70f6289ea0fb738db6dd914c4a7fd86e242743af
Matt M <42874279+mmarkewicz@users.noreply.github.com>
2019-10-09 09:20:20 -0400

    customer composition

---
+++ U1-M2-Summative-Markewicz-Matthew/CustomerComposition/src/main/java/com/company/Address.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Address {
+    private String streetOne;
+    private String streetTwo;
+    private String city;
+    private String state;
+    private int zip;
+}
