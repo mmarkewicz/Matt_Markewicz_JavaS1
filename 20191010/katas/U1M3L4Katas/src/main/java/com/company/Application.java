42867a4d915231c020c3471d2d9f9a0c79d58ba3
Matt M <42874279+mmarkewicz@users.noreply.github.com>
2019-10-09 12:40:12 -0400

    exception katas

---
+++ 20191010/katas/U1M3L4Katas/src/main/java/com/company/Application.java
@@ -0,0 +1,34 @@
+package com.company;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class Application {
+
+    public static void main(String[] args) {
+
+        try {
+            System.out.println("Current saved animals file:");
+            Pets.readPetsFromFile();
+
+            int petIndex = Pets.choosePet();
+
+            String chosenPet = Pets.retrievePet(petIndex);
+
+            Pets.writePetToFile(chosenPet);
+
+            System.out.println("New saved animals file:");
+            Pets.readPetsFromFile();
+        } catch (NumberFormatException e) {
+            System.out.println("Please enter a valid number.");
+        } catch (FileNotFoundException e) {
+            System.out.println("The following file does not seem to exist: " + e.getMessage());
+        } catch (IOException e) {
+            System.out.println("IO Exception");
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Please choose an available pet.");;
+        } finally {
+            System.out.println("Have a nice day!");
+        }
+    }
+}
