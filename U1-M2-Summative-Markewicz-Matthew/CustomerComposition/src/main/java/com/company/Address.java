77420fa4bdc25d4a958e2990177f55d7754284ad
Matt M <42874279+mmarkewicz@users.noreply.github.com>
2019-10-09 09:31:29 -0400

    summative 2 coimplete

--- U1-M2-Summative-Markewicz-Matthew/CustomerComposition/src/main/java/com/company/Address.java
+++ U1-M2-Summative-Markewicz-Matthew/CustomerComposition/src/main/java/com/company/Address.java
@@ -6,4 +6,44 @@ public class Address {
     private String city;
     private String state;
     private int zip;
+
+    public String getStreetOne() {
+        return streetOne;
+    }
+
+    public void setStreetOne(String streetOne) {
+        this.streetOne = streetOne;
+    }
+
+    public String getStreetTwo() {
+        return streetTwo;
+    }
+
+    public void setStreetTwo(String streetTwo) {
+        this.streetTwo = streetTwo;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public int getZip() {
+        return zip;
+    }
+
+    public void setZip(int zip) {
+        this.zip = zip;
+    }
 }
