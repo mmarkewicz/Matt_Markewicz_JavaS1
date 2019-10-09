022df5712f43795e5795a488b96b0c8ca77baf6d
Matt M <42874279+mmarkewicz@users.noreply.github.com>
2019-10-09 18:57:13 -0400

    pbd complete

--- 20191010/1.3.5/pbd/MatthewMarkewiczU1M3L5PBD/src/main/java/com/company/ArrayLists184.java
+++ 20191010/1.3.5/pbd/MatthewMarkewiczU1M3L5PBD/src/main/java/com/company/ArrayLists184.java
@@ -1,4 +1,43 @@
 package com.company;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.Scanner;
+
 public class ArrayLists184 {
+    public static void main(String[] args) {
+        Random rand = new Random();
+        Scanner scanner = new Scanner(System.in);
+
+        List<Integer> arrList = new ArrayList<>();
+        boolean isArrayFilled = false;
+
+        while(!isArrayFilled) {
+            int randomNum = rand.nextInt(50) + 1;
+            arrList.add(randomNum);
+            if (arrList.size() == 10) {
+                isArrayFilled = true;
+            }
+        }
+
+        System.out.println("ArrayList: " + arrList);
+        System.out.println("Value to find: ");
+        int userInt = scanner.nextInt();
+        boolean isInArr = false;
+
+        for (int num : arrList) {
+            if (num == userInt) {
+                isInArr = true;
+            }
+        }
+
+        if (isInArr) {
+            int index = arrList.lastIndexOf(userInt);
+            System.out.println(userInt + " is in the ArrayList at slot " + index);
+        } else {
+            System.out.println(userInt + " is not in the ArrayList");
+        }
+
+    }
 }
