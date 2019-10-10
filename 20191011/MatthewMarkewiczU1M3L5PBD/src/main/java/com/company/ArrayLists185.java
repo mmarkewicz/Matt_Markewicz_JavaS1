d8d1bcfd4ff3e309ca763240320b24139e8498db
Matt M <42874279+mmarkewicz@users.noreply.github.com>
2019-10-10 18:35:38 -0400

    pbd

--- 20191011/MatthewMarkewiczU1M3L5PBD/src/main/java/com/company/ArrayLists185.java
+++ 20191011/MatthewMarkewiczU1M3L5PBD/src/main/java/com/company/ArrayLists185.java
@@ -1,4 +1,27 @@
 package com.company;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 public class ArrayLists185 {
+    public static void main(String[] args) {
+        Random random = new Random();
+        List<Integer> list = new ArrayList<>();
+
+        while(list.size() < 10) {
+            int rand = random.nextInt(100) + 1;
+            list.add(rand);
+        }
+
+        int max = list.get(0);
+
+        for(int i = 0; i < list.size(); i++) {
+            if (list.get(i) > max) {
+                max = list.get(i);
+            }
+        }
+
+        System.out.println(max + " is the largest integer in the array");
+    }
 }
