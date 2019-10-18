bb6b7c2e01dd78e6ddef6eddbb2a58be345d4915
Matt M <42874279+mmarkewicz@users.noreply.github.com>
2019-10-18 08:42:12 -0400

    month converter

--- 20191018/month-converter/src/test/java/com/company/monthconverter/controller/MonthConverterControllerTest.java
+++ 20191018/month-converter/src/test/java/com/company/monthconverter/controller/MonthConverterControllerTest.java
@@ -1,4 +1,30 @@
 package com.company.monthconverter.controller;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(MonthConverterController.class)
 public class MonthConverterControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void shouldReturnMonthNameBasedOnPathVar() throws Exception {
+        mockMvc.perform(get("/month/3"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().string("March"));
+    }
+
 }
