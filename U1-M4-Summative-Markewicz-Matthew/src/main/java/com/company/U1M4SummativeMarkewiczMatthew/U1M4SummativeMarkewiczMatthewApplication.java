a8246356768ccaf5b7514ab91a97ecc49e79a582
Matt M <42874279+mmarkewicz@users.noreply.github.com>
2019-10-21 08:54:33 -0400

    summative

---
+++ U1-M4-Summative-Markewicz-Matthew/src/main/java/com/company/U1M4SummativeMarkewiczMatthew/U1M4SummativeMarkewiczMatthewApplication.java
@@ -0,0 +1,13 @@
+package com.company.U1M4SummativeMarkewiczMatthew;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class U1M4SummativeMarkewiczMatthewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(U1M4SummativeMarkewiczMatthewApplication.class, args);
+	}
+
+}
---
+++ U1-M4-Summative-Markewicz-Matthew/src/test/java/com/company/U1M4SummativeMarkewiczMatthew/U1M4SummativeMarkewiczMatthewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.U1M4SummativeMarkewiczMatthew;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class U1M4SummativeMarkewiczMatthewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
