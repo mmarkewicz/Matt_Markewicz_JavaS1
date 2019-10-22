3f9a018e959e2df732e57f0c49408bab64ac8d60
Matt M <42874279+mmarkewicz@users.noreply.github.com>
2019-10-22 15:44:22 -0400

    sql assignment description

---
+++ 20191023/assessment-sql-matthew-markewicz.sql
@@ -0,0 +1,40 @@
+-- northwind
+-- What are the categories of products in the database?
+-- What products are made by Dell?
+-- List all the orders shipped to Pennsylvania
+-- List the first name and last name of all employees with last names that start with w
+-- List all customers from zipcodes that start with 55
+-- List all customers from zipcodes that end with 0
+-- List the first name, last name, and email for all customers with a .org email
+-- List the first name, last name, and phone number for all customers from the 202 area code
+-- List the order id for each order placed by George Wilson
+-- List all the products and quantities associated with order 4003
+
+-- car_lot
+
+-- add the following cars to the inventory
+-- 2012 Red Honda Accord
+-- 2017 Black Chevy Impala
+-- 2019 Silver Ford F-150
+-- 2020 White Subaru Outback
+-- 2015 Silver Ford Mustang
+-- 2018 Blue Honda Ridgeline
+-- 2017 Gray Chevy Silverado
+
+-- make the following updates to the database 
+-- Change all Hondas to Black
+-- Change 'Chevy' to 'Cheverolt'
+-- Change all 2020 model years to 2019
+
+-- delete the following
+-- Delete alll blue inventory
+-- Delete all Fords
+-- Delete all cars from 2012 and 2017
+
+
+
+
+
+
+
+
