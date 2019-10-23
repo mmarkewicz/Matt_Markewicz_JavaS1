858d24e651046d1d18b5a5f3d83a3c684eac7d5d
Matt M <42874279+mmarkewicz@users.noreply.github.com>
2019-10-23 14:30:00 -0400

    coffeDAO

---
+++ 20191024/CoffeeInventoryDAO-Matthew-Markewicz/src/main/java/com/company/CoffeeInventoryDAOMatthewMarkewicz/CoffeeInventoryDaoMatthewMarkewiczApplication.java
@@ -0,0 +1,13 @@
+package com.company.CoffeeInventoryDAOMatthewMarkewicz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoffeeInventoryDaoMatthewMarkewiczApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoffeeInventoryDaoMatthewMarkewiczApplication.class, args);
+	}
+
+}
