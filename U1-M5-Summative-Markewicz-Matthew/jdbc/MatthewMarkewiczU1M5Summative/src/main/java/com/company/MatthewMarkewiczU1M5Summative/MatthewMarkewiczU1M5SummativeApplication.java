823ee3b93c413a0f4bb5e2ab4c56a45a1b026da9
Matt M <42874279+mmarkewicz@users.noreply.github.com>
2019-10-26 13:32:18 -0400

    summative projects generated

---
+++ U1-M5-Summative-Markewicz-Matthew/jdbc/MatthewMarkewiczU1M5Summative/src/main/java/com/company/MatthewMarkewiczU1M5Summative/MatthewMarkewiczU1M5SummativeApplication.java
@@ -0,0 +1,13 @@
+package com.company.MatthewMarkewiczU1M5Summative;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatthewMarkewiczU1M5SummativeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MatthewMarkewiczU1M5SummativeApplication.class, args);
+	}
+
+}
---
+++ U1-M5-Summative-Markewicz-Matthew/jpa/MatthewMarkewiczU1M5Summative/src/test/java/com/company/MatthewMarkewiczU1M5Summative/MatthewMarkewiczU1M5SummativeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.MatthewMarkewiczU1M5Summative;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatthewMarkewiczU1M5SummativeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
