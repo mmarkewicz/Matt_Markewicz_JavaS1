71f4f90afd73e9bb249df81cf47214ac07e346f3
Matt M <42874279+mmarkewicz@users.noreply.github.com>
2019-10-27 17:16:01 -0400

    coffeeinventory initializer

---
+++ 20191028/MatthewMarkewiczCoffeeInventoryJPARepository/src/main/java/com/company/MatthewMarkewiczCoffeeInventoryJPARepository/MatthewMarkewiczCoffeeInventoryJpaRepositoryApplication.java
@@ -0,0 +1,13 @@
+package com.company.MatthewMarkewiczCoffeeInventoryJPARepository;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatthewMarkewiczCoffeeInventoryJpaRepositoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MatthewMarkewiczCoffeeInventoryJpaRepositoryApplication.class, args);
+	}
+
+}
---
+++ 20191028/MatthewMarkewiczCoffeeInventoryJPARepository/src/test/java/com/company/MatthewMarkewiczCoffeeInventoryJPARepository/MatthewMarkewiczCoffeeInventoryJpaRepositoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.MatthewMarkewiczCoffeeInventoryJPARepository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatthewMarkewiczCoffeeInventoryJpaRepositoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
