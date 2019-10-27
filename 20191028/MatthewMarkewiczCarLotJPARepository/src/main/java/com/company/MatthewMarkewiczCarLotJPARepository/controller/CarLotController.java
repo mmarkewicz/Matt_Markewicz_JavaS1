0787541316274dbbb9cd56ecc706375dc971e996
Matt M <42874279+mmarkewicz@users.noreply.github.com>
2019-10-27 17:51:28 -0400

    add response status

--- 20191028/MatthewMarkewiczCarLotJPARepository/src/main/java/com/company/MatthewMarkewiczCarLotJPARepository/controller/CarLotController.java
+++ 20191028/MatthewMarkewiczCarLotJPARepository/src/main/java/com/company/MatthewMarkewiczCarLotJPARepository/controller/CarLotController.java
@@ -3,6 +3,7 @@ package com.company.MatthewMarkewiczCarLotJPARepository.controller;
 import com.company.MatthewMarkewiczCarLotJPARepository.dao.CarRepository;
 import com.company.MatthewMarkewiczCarLotJPARepository.dto.Car;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -14,22 +15,26 @@ public class CarLotController {
     private CarRepository carRepository;
 
     @RequestMapping(value = "/add", method = RequestMethod.POST)
+    @ResponseStatus(HttpStatus.OK)
     public Car createCar(@RequestBody Car car) {
         carRepository.save(car);
         return car;
     }
 
     @RequestMapping(value = "/getByMake/{make}", method = RequestMethod.GET)
+    @ResponseStatus(HttpStatus.OK)
     public List<Car> getCarByMake(@PathVariable String make) {
         return carRepository.findByMake(make);
     }
 
     @RequestMapping(value = "/getByColor/{color}", method = RequestMethod.GET)
+    @ResponseStatus(HttpStatus.OK)
     public List<Car> getCarByColor(@PathVariable String color) {
         return carRepository.findByColor(color);
     }
 
     @RequestMapping(value = "/getByMakeAndColor/{make}/{color}", method = RequestMethod.GET)
+    @ResponseStatus(HttpStatus.OK)
     public List<Car> getCarByMakeAndColor(@PathVariable String make, @PathVariable String color) {
         return carRepository.findByMakeAndColor(make, color);
     }
