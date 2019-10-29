38f847cd8b96a65217eb858d747800a80238fdca
Matt M <42874279+mmarkewicz@users.noreply.github.com>
2019-10-29 12:33:42 -0400

    author impl

--- U1-M5-Summative-Markewicz-Matthew/jdbc/MatthewMarkewiczU1M5Summative/src/main/java/com/company/MatthewMarkewiczU1M5Summative/dao/AuthorDaoImpl.java
+++ U1-M5-Summative-Markewicz-Matthew/jdbc/MatthewMarkewiczU1M5Summative/src/main/java/com/company/MatthewMarkewiczU1M5Summative/dao/AuthorDaoImpl.java
@@ -1,7 +1,99 @@
 package com.company.MatthewMarkewiczU1M5Summative.dao;
 
+import com.company.MatthewMarkewiczU1M5Summative.models.Author;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
 
 @Repository
 public class AuthorDaoImpl implements AuthorDao {
+
+    // prepared statements
+    private static final String INSERT_AUTHOR_SQL =
+            "INSERT INTO author (first_name, last_name, street, city, state, postal_code, phone, email) VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
+
+    private static final String SELECT_AUTHOR_SQL =
+            "SELECT * FROM author WHERE author_id = ?";
+
+    private static final String SELECT_ALL_AUTHORS_SQL =
+            "SELECT * FROM author";
+
+    private static final String DELETE_AUTHOR_SQL =
+            "DELETE FROM author WHERE author_id = ?";
+
+    private static final String UPDATE_AUTHOR_SQL =
+            "UPDATE author SET first_name = ?, last_name = ?, street = ?, city = ?, state = ?, postal_code = ?, phone = ?, email = ? WHERE author_id = ?";
+
+    private JdbcTemplate jdbcTemplate;
+
+    public AuthorDaoImpl(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    @Override
+    public Author getAuthor(int id) {
+        return jdbcTemplate.queryForObject(SELECT_AUTHOR_SQL, this::mapRowToAuthor, id);
+    }
+
+    @Override
+    public List<Author> getAllAuthors() {
+        return jdbcTemplate.query(SELECT_ALL_AUTHORS_SQL, this::mapRowToAuthor);
+    }
+
+    @Override
+    @Transactional
+    public Author addAuthor(Author author) {
+        jdbcTemplate.update(INSERT_AUTHOR_SQL,
+                author.getFirst_name(),
+                author.getLast_name(),
+                author.getStreet(),
+                author.getCity(),
+                author.getState(),
+                author.getPostal_code(),
+                author.getPhone(),
+                author.getEmail()
+                );
+
+        int id = jdbcTemplate.queryForObject("SELECT last_insert_author_id()", Integer.class);
+        author.setAuthor_id(id);
+        return author;
+    }
+
+    @Override
+    public void updateAuthor(Author author) {
+        jdbcTemplate.update(INSERT_AUTHOR_SQL,
+                author.getAuthor_id(),
+                author.getFirst_name(),
+                author.getLast_name(),
+                author.getStreet(),
+                author.getCity(),
+                author.getState(),
+                author.getPostal_code(),
+                author.getPhone(),
+                author.getEmail()
+        );
+    }
+
+    @Override
+    public void deleteAuthor(int id) {
+        jdbcTemplate.update(DELETE_AUTHOR_SQL, id);
+    }
+
+    private Author mapRowToAuthor(ResultSet rs, int rowNum) throws SQLException {
+        Author author = new Author();
+        author.setAuthor_id(rs.getInt("author_id"));
+        author.setFirst_name(rs.getString("first_name"));
+        author.setLast_name(rs.getString("last_name"));
+        author.setStreet(rs.getString("street"));
+        author.setCity(rs.getString("city"));
+        author.setState(rs.getString("state"));
+        author.setPostal_code(rs.getString("postal_code"));
+        author.setPhone(rs.getString("phone"));
+        author.setEmail(rs.getString("email"));
+        return author;
+    }
 }
