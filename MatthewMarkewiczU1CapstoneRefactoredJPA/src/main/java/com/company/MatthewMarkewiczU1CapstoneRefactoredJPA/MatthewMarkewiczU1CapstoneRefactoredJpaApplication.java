4f0876f2323483df139e297615355f8c8644c159
Matt M <42874279+mmarkewicz@users.noreply.github.com>
2019-11-06 12:14:21 -0500

    start jpa refactor

---
+++ MatthewMarkewiczU1CapstoneRefactoredJPA/src/main/java/com/company/MatthewMarkewiczU1CapstoneRefactoredJPA/MatthewMarkewiczU1CapstoneRefactoredJpaApplication.java
@@ -0,0 +1,13 @@
+package com.company.MatthewMarkewiczU1CapstoneRefactoredJPA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatthewMarkewiczU1CapstoneRefactoredJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MatthewMarkewiczU1CapstoneRefactoredJpaApplication.class, args);
+	}
+
+}
---
+++ MatthewMarkewiczU1CapstoneRefactoredJPA/src/test/java/com/company/MatthewMarkewiczU1CapstoneRefactoredJPA/MatthewMarkewiczU1CapstoneRefactoredJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.MatthewMarkewiczU1CapstoneRefactoredJPA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatthewMarkewiczU1CapstoneRefactoredJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
