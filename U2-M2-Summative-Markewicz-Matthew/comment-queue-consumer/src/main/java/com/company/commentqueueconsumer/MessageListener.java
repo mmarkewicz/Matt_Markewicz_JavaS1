8a330cb3477ba940d3fad36094abb3671671a164
Matt M <42874279+mmarkewicz@users.noreply.github.com>
2019-11-27 14:53:41 -0500

    comment queue added, testing needed

--- U2-M2-Summative-Markewicz-Matthew/comment-queue-consumer/src/main/java/com/company/commentqueueconsumer/MessageListener.java
+++ U2-M2-Summative-Markewicz-Matthew/comment-queue-consumer/src/main/java/com/company/commentqueueconsumer/MessageListener.java
@@ -1,5 +1,7 @@
 package com.company.commentqueueconsumer;
 
+import com.company.commentqueueconsumer.feign.CommentServiceFeign;
+import com.company.commentqueueconsumer.model.Comment;
 import com.company.commentqueueconsumer.util.message.CommentMessage;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,10 +11,17 @@ import org.springframework.stereotype.Service;
 public class MessageListener {
 
     @Autowired
-    
+    CommentServiceFeign commentServiceFeign;
 
     @RabbitListener(queues = CommentQueueConsumerApplication.QUEUE_NAME)
     public void receiveMessage(CommentMessage msg) {
+        Comment comment = new Comment();
+        comment.setCommenterName(msg.getCommenterName());
+        comment.setComment(msg.getComment());
+        comment.setCreateDate(msg.getCreateDate());
+        comment.setPostId(msg.getPostId());
 
+        commentServiceFeign.postComment(comment);
+        System.out.println("Comment Posted");
     }
 }
