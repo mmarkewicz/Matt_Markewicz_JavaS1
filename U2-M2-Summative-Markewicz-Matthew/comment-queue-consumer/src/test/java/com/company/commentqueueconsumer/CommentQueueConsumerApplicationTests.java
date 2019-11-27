02b4003f480f4bcb7225d51e0ec184e5b3e66f24
Matt M <42874279+mmarkewicz@users.noreply.github.com>
2019-11-27 14:20:09 -0500

    created comment queue consumer

---
+++ U2-M2-Summative-Markewicz-Matthew/comment-queue-consumer/src/test/java/com/company/commentqueueconsumer/CommentQueueConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.commentqueueconsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommentQueueConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
