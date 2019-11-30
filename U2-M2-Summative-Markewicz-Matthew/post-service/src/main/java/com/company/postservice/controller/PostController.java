5efac847bd967516eab4a64ed883bf4e4e819ce0
Matt M <42874279+mmarkewicz@users.noreply.github.com>
2019-11-30 18:58:06 -0500

    update queue

--- U2-M2-Summative-Markewicz-Matthew/post-service/src/main/java/com/company/postservice/controller/PostController.java
+++ U2-M2-Summative-Markewicz-Matthew/post-service/src/main/java/com/company/postservice/controller/PostController.java
@@ -12,43 +12,36 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@CacheConfig(cacheNames = {"posts"})
 public class PostController {
 
     @Autowired
     PostServiceLayer service;
 
-    @Cacheable
     @GetMapping("/posts")
     public List<Post> getAllPosts() {
         return service.getAllPosts();
     }
 
-    @Cacheable
     @GetMapping("/post/id/{id}")
     public Post getPostById(@PathVariable int id) {
         return service.getPostById(id);
     }
 
-    @Cacheable
     @GetMapping("/post/name/{posterName}")
     public List<Post> getPostsByPosterName(@PathVariable String posterName) {
         return service.getPostsByPoster(posterName);
     }
 
-    @CachePut(key = "#results.getId()")
     @PostMapping("/posts")
     public Post postPost(@RequestBody Post post) {
         return  service.postPost(post);
     }
 
-    @CacheEvict(key = "#posts.getId()")
     @PutMapping("/posts")
     public void putPost(@RequestBody Post post) {
         service.updatePost(post);
     }
 
-    @CacheEvict
     @DeleteMapping("/post/{id}")
     public void deletePost(@PathVariable int id) {
         service.deletePost(id);
