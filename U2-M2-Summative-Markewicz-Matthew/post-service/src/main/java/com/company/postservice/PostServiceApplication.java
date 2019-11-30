67ad77fe889bf2deb486caadb908a7d93bae14ee
Matt M <42874279+mmarkewicz@users.noreply.github.com>
2019-11-30 14:53:22 -0500

    added cacheign

--- U2-M2-Summative-Markewicz-Matthew/post-service/src/main/java/com/company/postservice/PostServiceApplication.java
+++ U2-M2-Summative-Markewicz-Matthew/post-service/src/main/java/com/company/postservice/PostServiceApplication.java
@@ -2,12 +2,14 @@ package com.company.postservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableEurekaClient
+@EnableCaching
 public class PostServiceApplication {
 
 	public static void main(String[] args) {
--- U2-M2-Summative-Markewicz-Matthew/post-service/src/main/java/com/company/postservice/controller/PostController.java
+++ U2-M2-Summative-Markewicz-Matthew/post-service/src/main/java/com/company/postservice/controller/PostController.java
@@ -3,41 +3,52 @@ package com.company.postservice.controller;
 import com.company.postservice.model.Post;
 import com.company.postservice.service.PostServiceLayer;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheConfig;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@CacheConfig(cacheNames = {"posts"})
 public class PostController {
 
     @Autowired
     PostServiceLayer service;
 
+    @Cacheable
     @GetMapping("/posts")
     public List<Post> getAllPosts() {
         return service.getAllPosts();
     }
 
+    @Cacheable
     @GetMapping("/post/id/{id}")
     public Post getPostById(@PathVariable int id) {
         return service.getPostById(id);
     }
 
+    @Cacheable
     @GetMapping("/post/name/{posterName}")
     public List<Post> getPostsByPosterName(@PathVariable String posterName) {
         return service.getPostsByPoster(posterName);
     }
 
+    @CachePut(key = "#results.getId()")
     @PostMapping("/posts")
     public Post postPost(@RequestBody Post post) {
         return  service.postPost(post);
     }
 
+    @CacheEvict(key = "#posts.getId()")
     @PutMapping("/posts")
     public void putPost(@RequestBody Post post) {
         service.updatePost(post);
     }
 
+    @CacheEvict
     @DeleteMapping("/post/{id}")
     public void deletePost(@PathVariable int id) {
         service.deletePost(id);
