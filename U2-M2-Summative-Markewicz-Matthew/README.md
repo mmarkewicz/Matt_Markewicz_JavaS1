1e2c1ebc259e4363c566f616e2a213c3573f6d92
Matt M <42874279+mmarkewicz@users.noreply.github.com>
2019-12-04 08:38:23 -0500

    update summative

--- U2-M2-Summative-Markewicz-Matthew/README.md
+++
@@ -1,3 +0,0 @@
-### TODO:
-
-- JSR303 Validation
--- U2-M2-Summative-Markewicz-Matthew/stwitter-service/src/main/java/com/company/stwitterservice/controller/StwitterController.java
+++ U2-M2-Summative-Markewicz-Matthew/stwitter-service/src/main/java/com/company/stwitterservice/controller/StwitterController.java
@@ -4,22 +4,27 @@ import com.company.stwitterservice.model.Post;
 import com.company.stwitterservice.model.PostViewModel;
 import com.company.stwitterservice.service.StwitterServiceLayer;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheConfig;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.List;
 
 @RestController
+@CacheConfig(cacheNames = {"posts"})
 public class StwitterController {
 
     @Autowired
     StwitterServiceLayer service;
 
+    @Cacheable
     @GetMapping("/posts/{id}")
     public Post getPost(@PathVariable int id) throws Exception {
         return service.getPost(id);
     }
 
+    @Cacheable
     @GetMapping("/posts/user/{posterName}")
     public List<Post> getPostsByUser(@PathVariable String posterName) throws Exception {
         return service.getPostsByPoster(posterName);
