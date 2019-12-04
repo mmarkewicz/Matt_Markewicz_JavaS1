c08df4e7abf09cdfea3788b28d1f8a81bd179156
Matt M <42874279+mmarkewicz@users.noreply.github.com>
2019-12-04 13:46:09 -0500

    update pt.1 of tutorial

---
+++ 20191205/baeldung-tutorial/RESTProducer/src/test/java/com/company/RESTProducer/RestProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.RESTProducer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestProducerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
