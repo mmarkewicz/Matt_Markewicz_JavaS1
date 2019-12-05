bb4df7129fe6825106f4da20ba3a11ea744095d9
Matt M <42874279+mmarkewicz@users.noreply.github.com>
2019-12-05 12:39:49 -0500

    update

---
+++ tutorials/security-tutorials/oath-tutorial/oauth-greeter/src/main/java/com/trilogyed/oauthgreeter/OauthGreeterApplication.java
@@ -0,0 +1,13 @@
+package com.trilogyed.oauthgreeter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OauthGreeterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OauthGreeterApplication.class, args);
+	}
+
+}
