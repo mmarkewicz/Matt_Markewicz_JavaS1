fb82a0f296adcf2358d9d3183065fe215402a0c8
Matt M <42874279+mmarkewicz@users.noreply.github.com>
2019-12-05 11:06:52 -0500

    secure-app

---
+++ tutorials/security-tutorials/spring-security-tutorial/secure-app/src/test/java/com/trilogyed/secureapp/SecureAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trilogyed.secureapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecureAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
