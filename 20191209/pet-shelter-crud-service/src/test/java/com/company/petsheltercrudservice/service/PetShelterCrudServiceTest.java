3aa363a2d9f6a28457a246360cdda6deee17a0df
Matt M <42874279+mmarkewicz@users.noreply.github.com>
2019-12-07 17:14:55 -0500

    update

--- 20191209/pet-shelter-crud-service/src/test/java/com/company/petsheltercrudservice/service/PetShelterCrudServiceTest.java
+++ 20191209/pet-shelter-crud-service/src/test/java/com/company/petsheltercrudservice/service/PetShelterCrudServiceTest.java
@@ -2,13 +2,14 @@ package com.company.petsheltercrudservice.service;
 
 import com.company.petsheltercrudservice.model.Pet;
 import org.junit.After;
-import org.junit.Before;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 
@@ -19,14 +20,16 @@ public class PetShelterCrudServiceTest {
     @Autowired
     PetShelterCrudService service;
 
-    @Before
+    @BeforeEach
     public void setUp() {
-        service.findAllPets().forEach(pet -> service.deletePetById(pet.getId()));
+        List<Pet> petList = service.findAllPets();
+        petList.forEach(pet -> service.deletePetById(pet.getId()));
     }
 
     @After
     public void tearDown() {
-        service.findAllPets().forEach(pet -> service.deletePetById(pet.getId()));
+        List<Pet> petList = service.findAllPets();
+        petList.forEach(pet -> service.deletePetById(pet.getId()));
     }
 
     @Test
@@ -39,5 +42,33 @@ public class PetShelterCrudServiceTest {
 
         assertEquals(pet.toString(), service.findPetById(pet.getId()).toString());
 
+        service.deletePetById(pet.getId());
+
+        assertEquals(service.findAllPets().size(), 0);
+    }
+
+    @Test
+    public void shouldGetAllPets() {
+        Pet pet = new Pet();
+        pet.setType("Test Type");
+        pet.setOwner("Test Owner");
+        pet.setName("Test Name");
+        service.createPet(pet);
+
+        assertEquals(service.findAllPets().size(), 1);
+    }
+
+    @Test
+    public void shouldUpdatePet() {
+        Pet pet = new Pet();
+        pet.setType("Test Type");
+        pet.setOwner("Test Owner");
+        pet.setName("Test Name");
+        pet = service.createPet(pet);
+
+        pet.setType("New Test Type");
+        service.updatePet(pet);
+
+        assertEquals(service.findPetById(pet.getId()).getType(), "New Test Type");
     }
 }
