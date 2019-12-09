c562a03a0ac8b8bd21a6b1f333ece97804118476
Matt M <42874279+mmarkewicz@users.noreply.github.com>
2019-12-09 11:13:10 -0500

    maven crud serviec

---
+++ 20191209/pet-shelter-maven/pet-shelter-crud-service/src/main/java/com/company/petsheltercrudservice/controller/PetShelterCrudController.java
@@ -0,0 +1,40 @@
+package com.company.petsheltercrudservice.controller;
+
+import com.company.petsheltercrudservice.model.Pet;
+import com.company.petsheltercrudservice.service.PetShelterCrudService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class PetShelterCrudController {
+
+    @Autowired
+    PetShelterCrudService service;
+
+    @GetMapping("/pets")
+    public List<Pet> getAllPets() {
+        return service.readAllPets();
+    }
+
+    @GetMapping("/pet/{id}")
+    public Pet getPetById(@PathVariable int id) {
+        return service.readPetById(id);
+    }
+
+    @PostMapping("/pets")
+    public Pet postPet(@RequestBody Pet pet) {
+        return service.createPet(pet);
+    }
+
+    @PutMapping("/pets")
+    public void updatePet(@RequestBody Pet pet) {
+        service.updatePet(pet);
+    }
+
+    @DeleteMapping("/pet/{id}")
+    public void deletePet(@PathVariable int id) {
+        service.deletePet(id);
+    }
+}
---
+++ 20191209/pet-shelter-maven/pet-shelter-crud-service/src/test/java/com/company/petsheltercrudservice/repository/PetShelterCrudRepositoryTest.java
@@ -0,0 +1,71 @@
+package com.company.petsheltercrudservice.repository;
+
+import com.company.petsheltercrudservice.model.Pet;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+
+@SpringBootTest
+@RunWith(SpringJUnit4ClassRunner.class)
+public class PetShelterCrudRepositoryTest {
+
+    @Autowired
+    PetShelterCrudRepository repository;
+
+    @Before
+    public void setUp() {
+        repository.findAll().forEach(pet -> repository.deleteById(pet.getId()));
+    }
+
+    @After
+    public void tearDown() {
+        repository.findAll().forEach(pet -> repository.deleteById(pet.getId()));
+    }
+
+    @Test
+    public void shouldAddGetDelete() {
+        Pet pet = new Pet();
+        pet.setType("Test Type");
+        pet.setOwner("Test Owner");
+        pet.setName("Test Name");
+        pet = repository.save(pet);
+
+        assertEquals(pet.toString(), repository.getOne(pet.getId()).toString());
+
+        repository.deleteById(pet.getId());
+
+        assertEquals(repository.findAll().size(), 0);
+    }
+
+    @Test
+    public void shouldGetAll() {
+        Pet pet = new Pet();
+        pet.setType("Test Type");
+        pet.setOwner("Test Owner");
+        pet.setName("Test Name");
+        repository.save(pet);
+
+        assertEquals(repository.findAll().size(), 1);
+    }
+
+    @Test
+    public void shouldUpdate() {
+        Pet pet = new Pet();
+        pet.setType("Test Type");
+        pet.setOwner("Test Owner");
+        pet.setName("Test Name");
+        pet = repository.save(pet);
+
+        pet.setName("New Test Name");
+        repository.save(pet);
+
+        assertEquals("New Test Name", repository.getOne(pet.getId()).getName());
+    }
+}
