15da91eeaeca9eb4dce19917185c06f6fddddcee
Matt M <42874279+mmarkewicz@users.noreply.github.com>
2019-12-09 09:04:46 -0500

    update

--- 20191209/pet-shelter-crud-service/src/main/java/com/company/petsheltercrudservice/repository/PetShelterCrudRepository.java
+++
@@ -1,9 +0,0 @@
-package com.company.petsheltercrudservice.repository;
-
-import com.company.petsheltercrudservice.model.Pet;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PetShelterCrudRepository extends JpaRepository<Pet, Integer> {
-}
