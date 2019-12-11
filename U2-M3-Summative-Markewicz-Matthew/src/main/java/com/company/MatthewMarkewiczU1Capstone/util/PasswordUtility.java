cca52fa833aca3a46199ea5b2ab180149bc4166d
Matt M <42874279+mmarkewicz@users.noreply.github.com>
2019-12-11 11:22:23 -0500

    update

--- U2-M3-Summative-Markewicz-Matthew/src/main/java/com/company/MatthewMarkewiczU1Capstone/util/PasswordUtility.java
+++ U2-M3-Summative-Markewicz-Matthew/src/main/java/com/company/MatthewMarkewiczU1Capstone/util/PasswordUtility.java
@@ -7,12 +7,9 @@ public class PasswordUtility {
 
     public static void main(String[] args) {
         PasswordEncoder enc = new BCryptPasswordEncoder();
-
         String password = "password";
-
         String encodedPassword = enc.encode(password);
-
         System.out.println(encodedPassword);
-
     }
+
 }
